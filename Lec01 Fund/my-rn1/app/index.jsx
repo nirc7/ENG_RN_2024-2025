c6bf418973d0de94d95fe85c53050afbe2087d67
nc <31476486+nirc7@users.noreply.github.com>
2025-03-18 12:55:58 +0200

    fund

---
+++ Lec01 Fund/my-rn1/app/index.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Ruppin2222</Text>
+    </View>
+  );
+}
