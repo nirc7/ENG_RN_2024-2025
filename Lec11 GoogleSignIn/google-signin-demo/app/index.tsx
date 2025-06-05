76eb080dd1dc0e04635ba3be16b75e011ead4e2b
nc <31476486+nirc7@users.noreply.github.com>
2025-06-05 15:30:32 +0300

    gsi1

---
+++ Lec11 GoogleSignIn/google-signin-demo/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>nir's demo.3</Text>
+    </View>
+  );
+}
