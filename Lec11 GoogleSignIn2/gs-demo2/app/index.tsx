a804e915ac2c2fb609fafd38af54ebcafe1411c2
nc <31476486+nirc7@users.noreply.github.com>
2025-06-12 15:36:38 +0300

    gs2

---
+++ Lec11 GoogleSignIn2/gs-demo2/app/index.tsx
@@ -0,0 +1,85 @@
+import { Button, Text, View } from "react-native";
+
+import {
+  GoogleSignin,
+  statusCodes,
+  isSuccessResponse,
+  isErrorWithCode,
+  GoogleSigninButton
+} from '@react-native-google-signin/google-signin';
+import { useEffect } from "react";
+
+
+export default function Index() {
+  useEffect(() => {
+    GoogleSignin.configure({
+      iosClientId: "943355076597-arvf1giip7nebeg599i9nholb0r7u3pp.apps.googleusercontent.com",
+      webClientId: "943355076597-0vr051ipp4ra9b5jai17gglfgbhrel07.apps.googleusercontent.com",
+      profileImageSize: 150
+    });
+  });
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+        console.log(response.data);
+        console.log(response.data.user);
+        console.log(response.data.user.email);
+        console.log(response.data.user.givenName);
+
+      } else {
+        // sign in was cancelled by user
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.signOut();
+      console.log('signed out');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>nir's demo.</Text>
+      <Text></Text>
+      <Button title="signin" onPress={signIn} />
+      <Text></Text>
+      <Button title="signout" onPress={signOut} />
+      <Text></Text>
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={ signIn }
+        disabled={false}
+      />
+    </View>
+  );
+}
